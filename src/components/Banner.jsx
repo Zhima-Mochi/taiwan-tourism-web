c7a05821561b04c0a7bedf1fcecca9df4d2c6459
Zhima-Mochi <yehgoter@gmail.com>
2022-01-10 11:29:00 +0800

    banner select

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,22 @@
-export default function Banner(){
+import { useState } from "react"
+import { ReactComponent as Search } from '../assets/img/search_white_24dp.svg';
+export default function Banner() {
+    const [region, setRegion] = useState('all');
+    const [keyword, setKeyword] = useState('');
     return (
-        <div className="banner flex flex-col items-center">
-            <h1 className="text-white decor tracking-widest df-ft-fm text-sm lg:text-lg mt-4 lg:mt-8">TRAVEL AROUND TAIWAN, BE FUN!</h1>
+        <div className="banner">
+            <div className="wrap flex flex-col items-center">
+                <h1 className="text-white decor tracking-widest df-ft-fm text-sm lg:text-lg mt-4 lg:mt-8 mb-4">TRAVEL AROUND TAIWAN, BE FUN!</h1>
+                <div className="w-full md:w-96 flex justify-center items-center">
+                    <input className="input w-full h-9 rounded-l df-ft-fm" placeholder="輸入目的地、景點或相關關鍵字"></input>
+                    <div className="btn main-bg-color h-9 rounded-r flex w-8 justify-center items-center">
+                        <Search />
+                    </div>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,27 +5,31 @@ export default function Header() {
     return (
         <div className="wrap">
             <div className="flex justify-between items-center header">
-                <NavLink to='/'>
-                    <div className="flex items-center">
-                        <img src={logo} alt="Logo" className="logo" />
-                        <div className="logo-word">樂遊台灣</div>
-                    </div>
-                </NavLink>
-                <div className="hidden md:block">
-                    <div className="flex justify-between">
-                        {Object.entries(navbar_items).map((item) => (
-                            <NavLink key={item[0]} to={'/' + item[1].eng}>
-                                <div className="navbar-box df-ft-fm">
-                                    <div className="text-xs hover-main-color">{item[1].name}</div>
-                                    <div className="text-xs navbar-color">{item[1].eng.replace('_', '').toUpperCase()}</div>
-                                </div>
-                            </NavLink>
-                        ))}
-                    </div>
+                <div className="flex-1">
+                    <NavLink to='/'>
+                        <div className="flex items-center">
+                            <img src={logo} alt="Logo" className="logo" />
+                            <div className="logo-word">樂遊台灣</div>
+                        </div>
+                    </NavLink>
                 </div>
                 <div>
+                    <div className="hidden md:block">
+                        <div className="flex justify-between">
+                            {Object.entries(navbar_items).map((item) => (
+                                <NavLink key={item[0]} to={'/' + item[1].eng}>
+                                    <div className="navbar-box df-ft-fm">
+                                        <div className="text-xs hover-main-color">{item[1].name}</div>
+                                        <div className="text-xs navbar-color">{item[1].eng.replace('_', '').toUpperCase()}</div>
+                                    </div>
+                                </NavLink>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-1">
                     <div className="md:hidden">
-                            
+
                     </div>
                 </div>
             </div>
