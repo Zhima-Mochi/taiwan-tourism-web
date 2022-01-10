49ff664f778902d402ca873712a8944c38687ad4
Zhima-Mochi <yehgoter@gmail.com>
2022-01-10 12:55:11 +0800

    banner region badge

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { ReactComponent as Search } from '../assets/img/search_white_24dp.svg';
+import { region_lst } from "../constants/region_lst";
 export default function Banner() {
     const [region, setRegion] = useState('all');
     const [keyword, setKeyword] = useState('');
@@ -7,16 +8,21 @@ export default function Banner() {
         <div className="banner">
             <div className="wrap flex flex-col items-center">
                 <h1 className="text-white decor tracking-widest df-ft-fm text-sm lg:text-lg mt-4 lg:mt-8 mb-4">TRAVEL AROUND TAIWAN, BE FUN!</h1>
-                <div className="w-full md:w-96 flex justify-center items-center">
-                    <input className="input w-full h-9 rounded-l df-ft-fm" placeholder="輸入目的地、景點或相關關鍵字"></input>
+                <div className="w-full md:w-96 flex justify-center items-center mb-4">
+                    <input className="input w-full h-9 rounded-l df-ft-fm" placeholder="輸入目的地、景點或相關關鍵字" onChange={(e) => setKeyword(e.target.value)}></input>
                     <div className="btn main-bg-color h-9 rounded-r flex w-8 justify-center items-center">
                         <Search />
                     </div>
                 </div>
-                <div>
-                    
+                <div className="w-full flex flex-wrap">
+                    <div className={"region-badge " + (region === 'all' ? 'region-badge-selected ' : ' ')} onClick={() => setRegion('all')}>全部</div>
+                    {region_lst.map(item => (
+                        <div key={item.id} className={"region-badge " + (region === item.id ? 'region-badge-selected ' : ' ')} onClick={() => setRegion(item.id)}>
+                            {item.name}
+                        </div>
+                    ))}
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
---
+++ src/constants/region_lst.js
@@ -0,0 +1,111 @@
+export const region_lst = [{
+        id: 'taipei',
+        eng: 'Taipei',
+        name: '臺北市'
+    },
+    {
+        id: 'newtaipei',
+        eng: 'NewTaipei',
+        name: '新北市'
+    },
+    {
+        id: 'taoyuan',
+        eng: 'Taoyuan',
+        name: '桃園市'
+    },
+    {
+        id: 'taichung',
+        eng: 'Taichung',
+        name: '臺中市'
+    },
+    {
+        id: 'tainan',
+        eng: 'Tainan',
+        name: '臺南市'
+    },
+    {
+        id: 'kaohsiung',
+        eng: 'Kaohsiung',
+        name: '高雄市'
+    },
+    {
+        id: 'keelung',
+        eng: 'Keelung',
+        name: '基隆市'
+    },
+    {
+        id: 'hsinchu',
+        eng: 'Hsinchu',
+        name: '新竹市'
+    },
+    {
+        id: 'hsinchucounty',
+        eng: 'HsinchuCounty',
+        name: '新竹縣'
+    },
+    {
+        id: 'miaolicounty',
+        eng: 'MiaoliCounty',
+        name: '苗栗縣'
+    },
+    {
+        id: 'changhuacounty',
+        eng: 'ChanghuaCounty',
+        name: '彰化縣'
+    },
+    {
+        id: 'nantoucounty',
+        eng: 'NantouCounty',
+        name: '南投縣'
+    },
+    {
+        id: 'yunlincounty',
+        eng: 'YunlinCounty',
+        name: '雲林縣'
+    },
+    {
+        id: 'chiayicounty',
+        eng: 'ChiayiCounty',
+        name: '嘉義縣'
+    },
+    {
+        id: 'chiayi',
+        eng: 'Chiayi',
+        name: '嘉義市'
+    },
+    {
+        id: 'pingtungcounty',
+        eng: 'PingtungCounty',
+        name: '屏東縣'
+    },
+    {
+        id: 'yilancounty',
+        eng: 'YilanCounty',
+        name: '宜蘭縣'
+    },
+    {
+        id: 'hualiencounty',
+        eng: 'HualienCounty',
+        name: '花蓮縣'
+    },
+    {
+        id: 'taitungcounty',
+        eng: 'TaitungCounty',
+        name: '臺東縣'
+    },
+    {
+        id: 'kinmencounty',
+        eng: 'KinmenCounty',
+        name: '金門縣'
+    },
+    {
+        id: 'penghucounty',
+        eng: 'PenghuCounty',
+        name: '澎湖縣'
+    },
+    {
+        id: 'lienchiangcounty',
+        eng: 'LienchiangCounty',
+        name: '連江縣'
+    },
+];
