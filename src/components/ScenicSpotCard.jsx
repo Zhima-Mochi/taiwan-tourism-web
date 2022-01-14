4ca0b1670ab47e74e92e4058830799693b757c26
Zhima-Mochi <yehgoter@gmail.com>
2022-01-14 18:09:04 +0800

    adjust

--- src/components/ScenicSpotCard.jsx
+++ src/components/ScenicSpotCard.jsx
@@ -2,17 +2,17 @@ import df_img from "../assets/img/default-image.png"
 import { ReactComponent as Schedule } from '../assets/img/schedule_black_24dp.svg';
 import { ReactComponent as Phone } from '../assets/img/phone_black_24dp.svg';
 import { ReactComponent as Place } from '../assets/img/place_black_24dp.svg';
-const text_setting = "font-sans text-xs font-semibold text-ellipsis whitespace-nowrap overflow-hidden w-5/6"
+const text_setting = "font-sans text-xs xl:text-sm font-semibold text-ellipsis whitespace-nowrap overflow-hidden w-5/6"
 export default function ScenicSpotCard(props) {
     if (props.data) {
         return (
-            <div className="bg-white card btn shadow-xl">
+            <div key={props.data.ScenicSpotID} className="bg-white card btn shadow-xl">
                 <img className="card-img" src={props.data.Picture.PictureUrl1 || df_img} alt="" />
-                <div className="card-content flex flex-col justify-between py-2 px-3  word-color">
-                    <h1 className="font-sans text-md font-bold text-ellipsis whitespace-nowrap overflow-hidden">{props.data.ScenicSpotName}</h1>
-                    <div className="flex items-center"> <Schedule className="h-3 w-3 mr-1 word-color"/><span className={text_setting}>{props.data.OpenTime}</span> </div>
-                    <div className="flex items-center"> <Phone className="h-3 w-3 mr-1 word-color"/><span className={text_setting}>{props.data.Phone}</span> </div>
-                    <div className="flex items-center"> <Place className="h-3 w-3 mr-1 word-color"/><span className={text_setting}>{props.data.Address}</span></div>
+                <div className="card-content flex flex-col justify-around py-2  px-4  word-color">
+                    <h1 className="font-sans text-sm  xl:text-lg font-bold text-ellipsis whitespace-nowrap overflow-hidden">{props.data.ScenicSpotName}</h1>
+                    <div className="flex items-center"> <Schedule className="h-3 w-3 mr-2 word-color" /><span className={text_setting}>{props.data.OpenTime}</span> </div>
+                    <div className="flex items-center"> <Phone className="h-3 w-3 mr-2 word-color" /><span className={text_setting}>{props.data.Phone}</span> </div>
+                    <div className="flex items-center"> <Place className="h-3 w-3 mr-2 word-color" /><span className={text_setting}>{props.data.Address}</span></div>
                 </div>
             </div>
         )
--- src/components/ScenicSpotCardList.jsx
+++ src/components/ScenicSpotCardList.jsx
@@ -9,21 +9,24 @@ export default function ScenicSpotCardList(props) {
     useEffect(() => { setFocusIdx(0) }, [scenicspot_data]);
     return (
         <div className="hidden sm:block">
-            <div className="flex items-center">
-                <div className="w-4 h-20">
-                    {focusIdx !== 0 && <div className="btn w-4 h-20 flex items-center" onClick={() => setFocusIdx(focusIdx - jump)}>
+            <div className="flex items-center justify-between">
+                <div>
+                    {focusIdx !== 0 ? <div className="btn w-4 h-40 flex items-center" onClick={() => setFocusIdx(focusIdx - jump)}>
                         <div className="text-center text-xl font-bold text-gray-500">〈</div>
-                    </div>}
+                    </div> :
+                        <div className="w-4"></div>}
                 </div>
                 <div className="flex justify-between">
                     <ScenicSpotCard data={scenicspot_data[focusIdx]} />
                     <ScenicSpotCard data={scenicspot_data[focusIdx + 1]} />
                     <ScenicSpotCard data={scenicspot_data[focusIdx + 2]} />
                 </div>
-                <div className="w-4 h-20">
-                    {focusIdx + 3 < scenicspot_data.length && <div className="btn w-4 h-20 flex items-center" onClick={() => setFocusIdx(focusIdx + jump)}>
+                <div>
+                    {focusIdx + 3 < scenicspot_data.length ? <div className="btn w-4 h-40 flex items-center" onClick={() => setFocusIdx(focusIdx + jump)}>
                         <div className="text-center text-xl font-bold text-gray-500">〉</div>
-                    </div>}
+                    </div> :
+                        <div className="w-4"></div>
+                    }
                 </div>
             </div>
         </div>
